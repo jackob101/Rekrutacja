b95c091dd914a21735174e81cc00e618f3a7b077
Jackob <pietrzyk.jakub001@gmail.com>
2022-01-23 18:55:40 +0100

    project structure

---
+++ src/test/java/com/jackob101/rekrutacja/RekrutacjaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackob101.rekrutacja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RekrutacjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
