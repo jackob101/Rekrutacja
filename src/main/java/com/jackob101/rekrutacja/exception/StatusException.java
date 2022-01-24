6f93735370323c25d93b6c050633eba623c73f34
Jackob <pietrzyk.jakub001@gmail.com>
2022-01-24 14:44:03 +0100

    Added protected constructor

--- src/main/java/com/jackob101/rekrutacja/exception/StatusException.java
+++ src/main/java/com/jackob101/rekrutacja/exception/StatusException.java
@@ -8,6 +8,10 @@ public class StatusException extends RuntimeException {
 
     private final HttpStatus status;
 
+    protected StatusException(){
+        status = HttpStatus.BAD_REQUEST;
+    }
+
     public StatusException(String message, HttpStatus status) {
         super(message);
         this.status = status;
